62bcd959b4699cb304b8a3c3753469ef9b83bcbd
Swapnil <ingle8042@gmail.com>
2026-02-04 13:57:38 +0530

    ABC

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -1,4 +1,4 @@
-from src.math_operations import add,sub
+from src.math_operations import add,sub,mul,div
 
 def test_add():
     assert add(2,3)==5
@@ -15,4 +15,4 @@ def test_mul():
 
 def test_div():
     assert div(10,2)==5
-    
+
